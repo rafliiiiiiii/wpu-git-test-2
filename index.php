20781c485de8a4c7a3a311099b17d5c2c8c82968
rafliiiiiiii <m.rafli10222087@sttcipasung.ac.id>
2024-11-21 19:51:47 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat omnis ex aspernatur dolorem saepe consectetur, odio assumenda vel, non maxime facilis. Quibusdam, ipsum. Minima illum impedit sequi officiis? Debitis, dicta?</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
