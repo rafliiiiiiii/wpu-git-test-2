5b4e37d487e3fdd737a50ff703660788bf33b2d5
rafliiiiiiii <m.rafli10222087@sttcipasung.ac.id>
2024-11-21 18:55:06 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat omnis ex aspernatur dolorem saepe consectetur, odio assumenda vel, non maxime facilis. Quibusdam, ipsum. Minima illum impedit sequi officiis? Debitis, dicta?</p>
 </body>
 </html>
